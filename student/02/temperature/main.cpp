53335d489e3b92cb3d18f9bfcc451bc28ced1a0d
Elias Nikkinen <elias.nikkinen@tuni.fi>
2022-01-13 19:15:57 +0200

    tehty temperature

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -3,10 +3,16 @@
 using namespace std;
 
 int main()
-{
+{   int temp = 0;
     cout << "Enter a temperature: ";
+    cin >> temp;
 
-    // Write your code here
+    float fahre = temp * 1.8 +32;
+    float cels = (temp - 32) /1.8;
+
+    cout << temp << " degrees Celsius is " << fahre << " degrees Fahrenheit" << endl;
+    cout << temp << " degrees Fahrenheit is " << cels << " degrees Celsius" << endl;
+    // Write your code her
 
     return 0;
 }
