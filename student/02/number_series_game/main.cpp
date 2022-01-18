9f884571b0ff83e6942abe3b84b387c3a5f6d9bc
Elias Nikkinen <elias.nikkinen@tuni.fi>
2022-01-18 15:29:23 +0200

    Syötteen lukeminen ja numeroiden tulostus

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
-
+using namespace std;
 
 int main()
 {
-    std::cout << "How many numbers would you like to have? ";
+    int number;
+    cout << "How many numbers would you like to have? ";
+    cin >> number;
+
+    int i = 1;
+    while(i <= number)
+    {
+        cout << "\n" << i << endl;
+        i++;
+    }
+
 
     return 0;
 }
