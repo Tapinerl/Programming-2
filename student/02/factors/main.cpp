2885a6fbc9de2431d07bdba58033b5c10eb18945
Elias Nikkinen <elias.nikkinen@tuni.fi>
2022-01-20 17:47:04 +0200

    factors

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -4,7 +4,37 @@ using namespace std;
 
 int main()
 {
-    cout << "Enter a positive number: ";
 
+    cout << "Enter a positive number: ";
+    int number;
+    cin >> number;
+
+    if (number <=0)
+    {
+        cout << "Only positive numbers accepted"<<endl;
+        return 0;
+    }
+
+    int shortest = number;
+    int a=1, b=1;
+
+    for (int i = 1; i < number; i++) {
+        int n = number/i;
+
+        if (i*n == number){
+            int distance = n-i;
+            if (distance < 0)
+            {
+                distance=-distance;
+            }
+            if (distance < shortest)
+            {
+                shortest = distance;
+                a=i;
+                b=n;
+            }
+        }
+    }
+    cout << number << " = " << a << " * " << b << endl;
     return 0;
 }
