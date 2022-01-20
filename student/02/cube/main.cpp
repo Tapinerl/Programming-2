d6b3841a8b62c2ed5bc32b836c0e57e8e4dd9adf
Elias Nikkinen <elias.nikkinen@tuni.fi>
2022-01-20 18:43:08 +0200

    juu

--- student/02/cube/main.cpp
+++ student/02/cube/main.cpp
@@ -4,13 +4,14 @@
 
 int main()
 {
-    std::cout << "Enter a number ";
+    std::cout << "Enter a number: ";
     int number;
     std::cin >> number;
 
-
     int cube = number*number*number;
-
+    if (number==0){
+        std::cout <<"The cube of " << number << " is " << cube <<"."<< std::endl;
+    }
     if (cube/number/number == number){
         std::cout <<"The cube of " << number << " is " << cube <<"."<< std::endl;
     }
