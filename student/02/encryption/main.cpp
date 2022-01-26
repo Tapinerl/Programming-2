a48f4bfcc364497145aece058226548938c042d0
Elias Nikkinen <elias.nikkinen@tuni.fi>
2022-01-26 16:35:22 +0200

    constants added, updated functions

--- student/02/encryption/main.cpp
+++ student/02/encryption/main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 
-using namespace std;
+const int ASCII_MIN = 97;
+const int ASCII_MAX = 122;
+const int KEY_MAX_LENGHT = 26;
+
+const std::string ERROR_LENGHT = "Error! The encryption key must contain 26 characters.";
+const std::string ERROR_LOWER_CASE = "Error! The encryption key must contain only lower case characters.";
+const std::string ERROR_ALPHABET = "Error! The encryption key must contain all alphabets a-z.";
+const std::string ERROR_TEXT_LOWER_CASE = "Error! The text to be encrypted must contain only lower case characters.";
+
+bool check_key_lenght(const std::string &key, const std::string &error_message);
+bool check_all_lower_case(const std::string &key, const std::string &error_message);
+bool check_all_alphabet(const std::string &key, const std::string &error_message);
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
+
     return 0;
+}
+
+
+bool check_key_lenght(const std::string &key, const std::string &error_message)
+{
+
+}
+
+bool check_all_lower_case(const std::string &key, const std::string &error_message)
+{
+
+}
+
+bool check_all_alphabet(const std::string &key, const std::string &error_message)
+{
+
 }
