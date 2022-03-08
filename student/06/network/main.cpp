0d27b83223cd53069724f816f33b56863d4ce176
Elias Nikkinen <elias.nikkinen@tuni.fi>
2022-03-08 16:56:00 +0200

    assigment done with from weekly rounds

--- student/06/network/main.cpp
+++ student/06/network/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <map>
 
 const std::string HELP_TEXT = "S = store id1 i2\nP = print id\n"
                               "C = count id\nD = depth id\n";
@@ -30,9 +31,59 @@ std::vector<std::string> split(const std::string& s,
     return result;
 }
 
+using NET = std::map<std::string, std::vector<std::string>>;
+
+void store(std::string key, std::string value, NET &network){
+    if(network.find(key) == network.end()){
+        network.insert({key, {}});
+    }
+    network.at(key).push_back(value);
+}
+
+void print(std::string key, NET const &network, std::string identation = ""){
+    if(network.find(key) == network.end() or network.at(key).empty()){
+        std::cout <<identation <<  key << std::endl;
+    }
+    else {
+        std::cout << identation << key << std::endl;
+        std::vector<std::string>::const_iterator iter = network.at(key).begin();
+        for(; iter != network.at(key).end(); iter++){
+            print(*iter, network, identation + "..");
+        }
+
+    }
+}
+
+int depth(std::string key, NET const &network){
+    int max_depth = 0;
+    if(network.find(key) != network.end()){
+        for(std::string id : network.at(key)){
+            if(depth(id, network) > max_depth){
+                max_depth = depth(id, network);
+            }
+        }
+    }
+    return max_depth + 1;
+}
+
+int count(std::string key, NET const &network){
+    if(network.find(key) == network.end() or network.at(key).empty()){
+        return 0;
+    }
+    else {
+        std::vector<std::string>::const_iterator iter = network.at(key).begin();
+        int size_at_level = network.at(key).size();
+        for(; iter != network.at(key).end(); iter++){
+            size_at_level += count(*iter, network);
+        }
+        return size_at_level;
+    }
+}
+
 int main()
 {
     // TODO: Implement the datastructure here
+    std::map<std::string, std::vector<std::string>> network = {};
 
 
     while(true)
@@ -61,6 +112,8 @@ int main()
             std::string id2 = parts.at(2);
 
             // TODO: Implement the command here!
+            store(id1, id2, network);
+
 
         }
         else if(command == "P" or command == "p")
@@ -73,6 +126,7 @@ int main()
             std::string id = parts.at(1);
 
             // TODO: Implement the command here!
+            print(id, network);
 
         }
         else if(command == "C" or command == "c")
@@ -85,6 +139,8 @@ int main()
             std::string id = parts.at(1);
 
             // TODO: Implement the command here!
+            std::cout << count(id, network) << std::endl;
+
 
         }
         else if(command == "D" or command == "d")
@@ -97,6 +153,7 @@ int main()
             std::string id = parts.at(1);
 
             // TODO: Implement the command here!
+            std::cout << depth(id, network) << std::endl;
 
         }
         else if(command == "Q" or command == "q")
