4caacdb42b1df7e8171ba05ae5549991ef1d0f33
Elias Nikkinen <elias.nikkinen@tuni.fi>
2022-03-28 18:22:11 +0300

    done

--- student/09/arrays/array_operations.cpp
+++ student/09/arrays/array_operations.cpp
@@ -1,1 +1,54 @@
 #include "array_operations.hh"
+
+int greatest_v1(int *itemptr, int size)
+{
+    int greatest = *itemptr;
+
+    for (int i = 0; i < size; i++){
+        int current = *(itemptr + 1);
+        if ( current > greatest){
+            greatest= current;
+        }
+    }
+    return greatest;
+}
+
+void copy(int *itemptr, int *endptr, int *targetptr)
+{
+    while(itemptr != endptr){
+        *targetptr = *itemptr;
+        itemptr++;
+        targetptr++;
+    }
+}
+
+int greatest_v2(int *itemptr, int *endptr)
+{
+    int greatest = *itemptr;
+
+    while(itemptr != endptr){
+        int current = *(itemptr++);
+        if(current > greatest){
+            greatest = current;
+        }
+    }
+    return greatest;
+}
+
+void reverse(int *leftptr, int *rightptr)
+{
+    rightptr--;
+
+    int temp;
+
+    while(leftptr < rightptr){
+        temp = *leftptr;
+        *leftptr = *rightptr;
+        *rightptr = temp;
+
+        leftptr++;
+        rightptr--;
+    }
+
+
+}
