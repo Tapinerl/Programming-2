ece8346c7ef0bb6a1c1914b97d8ae5ca3701daf3
Elias Nikkinen <elias.nikkinen@tuni.fi>
2022-04-12 12:43:42 +0300

    bmi assigment done

---
+++ student/11/bmi/mainwindow.cpp
@@ -0,0 +1,60 @@
+#include "mainwindow.hh"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow),
+    weight_(1.0),
+    height_(1.0)
+{
+    ui->setupUi(this);
+
+    connect(ui->countButton, SIGNAL(clicked()), this, SLOT(count()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+
+}
+
+void MainWindow::on_heightLineEdit_textChanged(const QString &arg1)
+{
+    height_ = arg1.toDouble();
+}
+
+void MainWindow::on_weightLineEdit_editingFinished()
+{
+    weight_ = ui->weightLineEdit->text().toDouble();
+}
+
+void MainWindow::count(){
+    if ( weight_ <= 0.0 || height_ <= 0.0 )
+    {
+        ui->infoTextBrowser->clear();
+        ui->resultLabel->clear();
+        return;
+    }
+    double bmi = ( weight_ ) / ( (height_ / 100) * (height_ / 100) );
+    if ( bmi < 1.0 || bmi > 200.0 )
+    {
+        ui->infoTextBrowser->clear();
+        ui->resultLabel->clear();
+        return;
+    }
+    QString str_bmi = QString::number(bmi);
+
+    if ( bmi < 18.5 )
+    {
+        ui->infoTextBrowser->setText("You are underweight.");
+    }
+    else if ( bmi < 25 )
+    {
+        ui->infoTextBrowser->setText("Your weight is normal.");
+    }
+    else
+    {
+        ui->infoTextBrowser->setText("You are overweight.");
+    }
+    ui->resultLabel->setText(str_bmi);
+}
