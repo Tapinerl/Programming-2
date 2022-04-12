6f7feb5caa05494877dd84461272a15f9929550c
Elias Nikkinen <elias.nikkinen@tuni.fi>
2022-04-12 14:08:35 +0300

    grading assigment done

---
+++ student/11/grading-gui/mainwindow.cpp
@@ -0,0 +1,32 @@
+#include "mainwindow.hh"
+#include "ui_mainwindow.h"
+#include "gradecounter.hh"
+#include <QString>
+#include <iostream>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_countPushButton_clicked()
+{
+     int n = ui->spinBoxN->value();
+     int g = ui->spinBoxG->value();
+     int p = ui->spinBoxP->value();
+     int e = ui->spinBoxE->value();
+
+    int result = count_final_grade(n,g,p,e);
+    const QString x = QString::number(result);
+    const QString text = "Total grade: " + x;
+
+    ui->textBrowser->setText(text);
+    return;
+}
